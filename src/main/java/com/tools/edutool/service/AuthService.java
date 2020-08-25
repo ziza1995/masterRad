39abca1174a2a36a08ae84bfc502835eb4bbb38f
Zeljko <zeljki.markovic15@gmail.com>
2020-08-25 23:27:40 +0200

    phase 1 fix

--- src/main/java/com/tools/edutool/service/AuthService.java
+++ src/main/java/com/tools/edutool/service/AuthService.java
@@ -43,7 +43,7 @@ public class AuthService {
         User user = new User();
         user.setUsername(registerRequest.getUsername());
         user.setEmail(registerRequest.getEmail());
-        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setPassword(registerRequest.getPassword());
         user.setCreated(Instant.now());
         user.setEnabled(true);
 
