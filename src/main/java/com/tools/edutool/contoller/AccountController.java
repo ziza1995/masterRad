c777b3fcca1fe42370d0fca1cf68abdb38fb92c6
Zeljko <zeljki.markovic15@gmail.com>
2020-08-31 00:14:54 +0200

    quick fix

--- src/main/java/com/tools/edutool/contoller/AccountController.java
+++ src/main/java/com/tools/edutool/contoller/AccountController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+import javax.servlet.http.HttpServletRequest;
+
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -15,6 +17,12 @@ import static org.springframework.http.HttpStatus.OK;
 public class AccountController {
 
     private final AccountService accountService;
+
+    @PostMapping()
+    public ResponseEntity<AccountDto> getAccountById(@RequestBody AccountDto  accountDto) {
+        AccountDto account = accountService.update(accountDto);
+        return new ResponseEntity<>(account, OK);
+    }
 
     @GetMapping("/{username}")
     public ResponseEntity<AccountDto> getAccountById(@PathVariable String  username) {
